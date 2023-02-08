e826af2927cb9804553718f9782daf6f2e879dbb
hoangVV.dev <hoangk62uet@gmail.com>
2023-02-08 23:54:20 +0700

    create recipes and shopping components

---
+++ src/app/recipes/recipes-list/recipes-list-item/recipes-list-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-list-item',
+  templateUrl: './recipes-list-item.component.html',
+  styleUrls: ['./recipes-list-item.component.scss']
+})
+export class RecipesListItemComponent {
+
+}
